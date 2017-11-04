4e4d613a6b1c0b0c2f40650856c8356c86a437e9
loudonclear <loudon_cohen@brown.edu>
2017-11-04 15:25:29 -0400

    smell/sight

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public float maxSpeed = 20;
+	public float movementForce = 100;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 move = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+
+		rb2d.velocity += move * movementForce;
+
+		float mag = rb2d.velocity.magnitude;
+
+		if (mag > maxSpeed) {
+			rb2d.velocity = maxSpeed * rb2d.velocity.normalized;
+		}
+	}
+		
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+		}
+
+		if (other.gameObject.CompareTag ("Enemy")) {
+			gameObject.SetActive (false);
+		}
+	}
+
+}
---
+++ Assets/SmellController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmellController : MonoBehaviour {
+
+    public Camera camera;
+
+    private bool smellMode = false;
+
+	// Use this for initialization
+	void Start () {
+        updateMode();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            updateMode();
+        }
+	}
+
+    private void updateMode()
+    {
+        if (smellMode)
+        {
+            camera.cullingMask = (1 << LayerMask.NameToLayer("SmellMode")) | (1 << LayerMask.NameToLayer("UI")) | (1 << LayerMask.NameToLayer("Default"));
+        }
+        else
+        {
+            camera.cullingMask = (1 << LayerMask.NameToLayer("SightMode")) | (1 << LayerMask.NameToLayer("UI")) | (1 << LayerMask.NameToLayer("Default"));
+        }
+
+        smellMode = !smellMode;
+    }
+}
